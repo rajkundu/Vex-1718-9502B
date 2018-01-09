157245ae0fb667bffb8822be1dd092989f0ab5a3
rajkundu <rjk368@gmail.com>
2018-01-08 22:09:06 -0500

    Updated to include custom library for remote controllers

--- include/main.h
+++ include/main.h
@@ -1,13 +1,15 @@
 /** @file main.h
  * @brief Header file for global functions
  *
- * Any experienced C or C++ programmer knows the importance of header files. For those who
- * do not, a header file allows multiple files to reference functions in other files without
- * necessarily having to see the code (and therefore causing a multiple definition). To make
- * a function in "opcontrol.c", "auto.c", "main.c", or any other C file visible to the core
- * implementation files, prototype it here.
+ * Any experienced C or C++ programmer knows the importance of header files. For
+ * those who do not, a header file allows multiple files to reference functions
+ * in other files without
+ * necessarily having to see the code (and therefore causing a multiple
+ * definition). To make a function in "opcontrol.c", "auto.c", "main.c", or any
+ * other C file visible to the core implementation files, prototype it here.
  *
- * This file is included by default in the predefined stubs in each VEX Cortex PROS Project.
+ * This file is included by default in the predefined stubs in each VEX Cortex
+ * PROS Project.
  *
  * Copyright (c) 2011-2016, Purdue University ACM SIGBots.
  * All rights reserved.
@@ -22,7 +24,8 @@
 
 #ifndef MAIN_H_
 
-// This prevents multiple inclusion, which isn't bad for this file but is good practice
+// This prevents multiple inclusion, which isn't bad for this file but is good
+// practice
 #define MAIN_H_
 
 #include <API.h>
@@ -37,8 +40,9 @@ unsigned char refreshRate = 20;
 extern "C" {
 #endif
 
-// A function prototype looks exactly like its declaration, but with a semicolon instead of
-// actual code. If a function does not match a prototype, compile errors will occur.
+// A function prototype looks exactly like its declaration, but with a semicolon
+// instead of actual code. If a function does not match a prototype, compile
+// errors will occur.
 
 // Prototypes for initialization, operator control and autonomous
 
